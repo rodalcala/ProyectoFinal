02d1530c995c4e423aecf0866003c58095cd40bc
Rocio Alcala <rro.alcala@gmail.com>
2022-08-07 20:04:56 -0300

    Interactuando con DOM para crear las cards

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ let carrito = []
 let stock
 const productos = []
 let totalCarrito
-class Producto {constructor (nombre, precio, cantidad) //funcion constructora de productos objetos
+class Producto {constructor (nombre, precio, cantidad, descripcion) //funcion constructora de productos objetos
                 {this.nombre = nombre
                 this.precio = precio
-                this.stock = parseInt(cantidad)}}
+                this.stock = parseInt(cantidad)
+                this.descripcion = descripcion}}
 
 
-const planta = new Producto ("planta", 400, 2) ; productos.push(planta)
-const kokedama = new Producto ("kokedama", 1000, 1) ; productos.push(kokedama)
-const maceta = new Producto ("maceta", 600, 0) ; productos.push(maceta)
+const planta = new Producto ("Planta", 400, 2, 'una planta') ; productos.push(planta)
+const kokedama = new Producto ("Kokedama", 1000, 1, 'una kokedama') ; productos.push(kokedama)
+const maceta = new Producto ("Maceta", 600, 0, '1 maceta') ; productos.push(maceta)
+
 
 //Definiendo las funciones del proceso de compra
 
@@ -101,3 +103,21 @@ let carritoSimplificado = carrito.map((el) => {
 console.log("Te mostramos tu carrito")
 console.table(carritoSimplificado)
 console.log("Esperamos que vuelvas pronto!!")
+
+//Creando las cards para cada producto
+
+const cards = document.getElementById('cards') 
+
+for (const producto of productos) {
+    const card = document.createElement("div") ;
+    card.innerHTML = `
+    <img src="${producto.nombre}.jpeg" class="card-img-top" alt="${producto.nombre}">
+    <div class="card-body">
+      <h5 class="card-title">${producto.nombre}</h5>
+      <p class="card-text">$${producto.precio}</p>
+      <a href="#" class="btn btn-dark">Agregar a carrito</a>
+    </div>` ;
+    card.className = "card col-12 col-md-4 col-lg-3" ;
+    cards.appendChild(card) ;
+}
+
