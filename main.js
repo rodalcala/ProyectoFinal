4f7409605b8b64ee2957aed348b9edd6956afad5
Rocio Alcala <rro.alcala@gmail.com>
2022-08-16 19:01:50 -0300

    Segundo preentrega Proyecto final

--- main.js
+++ main.js
@@ -1,113 +1,124 @@
-
-
-//Definiendo variables, objetos y arrays
-
-let carrito = []
-//agregar verificar que el storage no tenga un carrito guardado y si es asi agregarlo al carrito actual
-let stock
-const productos = []
-let totalCarrito
-class Producto {constructor (nombre, precio, cantidad, descripcion, imagen) //funcion constructora de productos objetos
+//Definiendo nuestros productos y su stock
+let productos = []
+class Producto {constructor (nombre, precio, stockInicial, imagen, can) //funcion constructora de productos objetos
                 {this.nombre = nombre
                 this.precio = precio
-                this.stock = parseInt(cantidad)
-                this.descripcion = descripcion
-                this.imagen = imagen}}
+                this.stock = parseInt(stockInicial)
+                this.imagen = imagen
+                this.can = can}}
+
+const planta = new Producto ("Planta", 400, 2, "planta.jpeg", 1) ; productos.push(planta)
+const kokedama = new Producto ("Kokedama", 1000, 1, "kokedama.jpeg", 1) ; productos.push(kokedama)
+const maceta = new Producto ("Maceta", 600, 2, "maceta.jpeg", 1) ; productos.push(maceta)
+
+//Definiendo variables y obteniendo elementos
+let stock 
+let totalCarrito ;
+const cards = document.getElementById("cards") 
+const tabla = document.getElementById("tbody")
+const tfoot = document.getElementById("tfoot")
 
 
-const planta = new Producto ("Planta", 400, 2, 'una planta', "planta.jpeg") ; productos.push(planta)
-const kokedama = new Producto ("Kokedama", 1000, 1, 'una kokedama', "kokedama.jpeg") ; productos.push(kokedama)
-const maceta = new Producto ("Maceta", 600, 2, '1 maceta', "maceta.jpeg") ; productos.push(maceta)
+//Verificar que el storage no tenga un carrito y stock de productos guardado y si es asi agregarlo al carrito actual
+let carrito = []
+if (localStorage.getItem("carrito")){carrito = JSON.parse(localStorage.getItem("carrito"))}
+if (localStorage.getItem("productos")){productos = JSON.parse(localStorage.getItem("productos"))}
+crearCarritoOnStorage() //Agrega a la lista de carrito los items que habia on storage
+totalCarrito = carrito.reduce((suma,el) => (suma + (el.precio*el.can)),0) //Calculo de total del carrito on storage
+mostrarTotalCarrito() //Muestra total
 
 
 //Definiendo las funciones del proceso de compra
-
 function actualizarStock(productoIngresado) {
     for (let producto of productos)
-    {if (productoIngresado.toLowerCase() === producto.nombre.toLowerCase()){producto.stock = producto.stock - 1 }}
+    {if (productoIngresado.toLowerCase() === producto.nombre.toLowerCase()){producto.stock = producto.stock - producto.can }}
 }
 
 function verificarStock(productoIngresado) {
     stock = productos.some((producto) => (producto.nombre.toLowerCase() === productoIngresado.toLowerCase() && producto.stock !== 0) ) 
-; return stock
+    return stock
+}
+
+function mostrarTotalCarrito (){    
+    if (carrito.length === 0){tfoot.innerHTML =`<th colspan="12"> No cargaste nada a tu carrito </th>`} else
+    {tfoot.innerHTML = `<th colspan="12"> El total de tu carrito es de $${totalCarrito}</th>`}
+ }
+
+function crearCarritoOnStorage() {
+    carrito.forEach(productoCarrito => {
+    const productoAgregado = document.createElement("tr")
+    productoAgregado.innerHTML =
+                    `<td>${productoCarrito.nombre}</td>
+					<td>$${productoCarrito.precio}</td>
+                    <td>${productoCarrito.can}</td>
+                    <td>$${productoCarrito.precio*productoCarrito.can}</td>
+					<td><button id="borrar${productoCarrito.nombre}" type="button" class="btn btn-dark">Borrar</button></td>`
+    tabla.appendChild(productoAgregado)
+})}
+
+function crearLineaEnCarrito(productoElegido) {
+    const productoAgregado = document.createElement("tr")
+    productoAgregado.innerHTML =
+                    `<td>${productoElegido.nombre}</td>
+					<td>$${productoElegido.precio}</td>
+                    <td>${productoElegido.can}</td>
+                    <td>$${productoElegido.precio*productoElegido.can}</td>
+					<td><button id="borrar${productoElegido.nombre}" type="button" class="btn btn-dark">Borrar</button></td>`
+    tabla.appendChild(productoAgregado)}
+
+function cambiarCantidad() {
+    productos.forEach (producto => {   
+    let cantidadProductos = document.getElementById(`cantidad-producto-${producto.nombre}`);
+        cantidadProductos.addEventListener("change", (e) => {
+        let nuevaCantidad = e.target.value;
+        producto.can = nuevaCantidad; })})
+}
+
+function agregarAlCarrito (productoElegido) {
+    stock = verificarStock(productoElegido.nombre) ; 
+    if(stock===false) {alert(`No hay mas stock de ${productoElegido.nombre} , lo sentimos mucho`)}
+    else {carrito.push(productoElegido)
+    crearLineaEnCarrito(productoElegido)
+    totalCarrito = carrito.reduce((suma,el) => (suma + (el.precio*el.can)),0)
+    //alert(`Agregaste ${productoElegido.nombre} al carrito`) pero mas lindos
+    actualizarStock(productoElegido.nombre)
+    mostrarTotalCarrito()
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    localStorage.setItem("productos",JSON.stringify(productos))
+}  
+}
+
+function BorrarDelCarrito(producto) { {carrito.splice(carrito.indexOf(producto),1)};
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    crearCarritoOnStorage()
 }
 
 //Creando las cards para cada producto
 
-const cards = document.getElementById('cards') 
-
 for (const producto of productos) {
     const card = document.createElement("div") ;
-    card.innerHTML = `
-    <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
-    <div class="card-body">
+    card.innerHTML = `<img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+      <div class="card-body">                 
       <h5 class="card-title">${producto.nombre}</h5>
       <p class="card-text">$${producto.precio}</p>
-      <a class="btn btn-dark" id="${producto.nombre}btn">Agregar ${producto.nombre} a carrito</a>
+      <input id="cantidad-producto-${producto.nombre}" type="number" placeholder="1" class="form-control" value="${producto.can}" min="1" max="1000" step="1" style="width: 50px;"/> 
+      <br>
+      <a class="btn btn-dark" href="#table" id="${producto.nombre}btn">Agregar ${producto.nombre} a carrito</a>
     </div>` ;
     card.className = "card col-12 col-md-4 col-lg-3" ;
     cards.appendChild(card) ;
 }
 
+
+
+//Agregando al carrito la card elegida
+
+cambiarCantidad()
 productos.forEach(producto => 
     {document.getElementById(`${producto.nombre}btn`).addEventListener("click",function(){agregarAlCarrito(producto)})});
 
- 
-function agregarAlCarrito (productoElegido) {
-    stock = verificarStock(productoElegido.nombre) ; 
-    if(stock===false) {alert(`No hay mas stock de ${productoElegido.nombre} , lo sentimos mucho`)}
-    else {carrito.push(productoElegido)
-    alert(`Agregaste ${productoElegido.nombre} al carrito`)
-    console.log("Elegiste "+productoElegido.nombre+", su precio es $"+productoElegido.precio);
-    totalCarrito = carrito.reduce((suma,el) => (suma + el.precio),0) ;
-    console.log("Tu carrito tiene $"+totalCarrito)
-    actualizarStock(productoElegido.nombre)
-    console.log("Te mostramos tu carrito") ; 
-    console.table(carrito) //agregar al local storage el carrito
-}
-}
-/* 
-const botonKokedamaCarrito = document.getElementById(`Kokedamabtn`) ;
-botonKokedamaCarrito.addEventListener("click",agregarKokedamaCarrito) ;
-const botonMacetaCarrito = document.getElementById(`Macetabtn`) ;
-botonMacetaCarrito.addEventListener("click",agregarMacetaCarrito) ;
-const botonPlantaCarrito = document.getElementById(`Plantabtn`) ;
-botonPlantaCarrito.addEventListener("click",agregarPlantaCarrito) ;
- 
-function agregarKokedamaCarrito () {
-    stock = verificarStock("kokedama") ; 
-    if(stock===false) {alert("No hay mas stock de Kokedama, lo sentimos mucho")}
-    else {carrito.push(kokedama)
-    alert("Agregaste Kokedama al carrito")
-    console.log("Elegiste Kokedama, su precio es $"+kokedama.precio);
-    totalCarrito = carrito.reduce((suma,el) => (suma + el.precio),0) ;
-    console.log("Tu carrito tiene $"+totalCarrito)
-    actualizarStock("kokedama")
-    console.log("Te mostramos tu carrito") ; 
-    console.table(carrito)}
-}
 
-function agregarPlantaCarrito () {
-    stock = verificarStock("Planta") ; 
-    if(stock===false) {alert("No hay mas stock de Planta, lo sentimos mucho")}
-    else {carrito.push(planta)
-    alert("Agregaste Planta al carrito")
-    console.log("Elegiste Planta, su precio es $"+planta.precio);
-    totalCarrito = carrito.reduce((suma,el) => (suma + el.precio),0) ;
-    console.log("Tu carrito tiene $"+totalCarrito)
-    actualizarStock("Planta")
-    console.log("Te mostramos tu carrito") ; console.table(carrito)}
-}
+//Borrar un producto del carrito
+carrito.forEach(producto => 
+    {document.getElementById(`borrar${producto.nombre}`).addEventListener("click",function(){BorrarDelCarrito(producto)})});
 
-function agregarMacetaCarrito () {
-    stock = verificarStock("Maceta") ; 
-    if(stock===false) {alert("No hay mas stock de Maceta, lo sentimos mucho")}
-    else {carrito.push(maceta)
-    alert("Agregaste Maceta al carrito")
-    console.log("Elegiste Maceta, su precio es $"+maceta.precio);
-    totalCarrito = carrito.reduce((suma,el) => (suma + el.precio),0) ;
-    console.log("Tu carrito tiene $"+totalCarrito)
-    actualizarStock("Maceta")
-    console.log("Te mostramos tu carrito") ; console.table(carrito)
-}
-} */
