90c8155c449cfd323b6c52f422794c9d167e6734
Rocio Alcala <rro.alcala@gmail.com>
2022-09-16 20:08:40 -0300

    Validacion de formulario

--- main.js
+++ main.js
@@ -9,45 +9,52 @@ const planta = new Producto ("Planta", 400, 2, "planta.jpeg", 1) ; productos.pus
 const kokedama = new Producto ("Kokedama", 1000, 2, "kokedama.jpeg", 1) ; productos.push(kokedama)
 const maceta = new Producto ("Maceta", 600, 2, "maceta.jpeg", 1) ; productos.push(maceta) */
 
-//Obteniendo nuestra base de productos desde JSON y renderizandolos
-async function obtenerJsonLocal(){
-    const JSON = await fetch('https://proyecto-final-two-red.vercel.app/productos.json') ;
-    const data = await JSON.json() ;
-    productos = data ;
-    console.log(productos)
-    verificarLocalStorage()
-    creandoCard(productos)
-}
-obtenerJsonLocal()
 
 //Definiendo variables y obteniendo elementos
 let productos = []
 let stock ;
 let totalCarrito ;
+let carrito = []
 const cards = document.getElementById("cards") 
 const tabla = document.getElementById("tbody")
 const tfoot = document.getElementById("tfoot")
 const terminarCompra = document.getElementById("terminarCompra")
-const filtro = document.getElementById("filtro") 
 const buscador = document.getElementById("buscar") 
-//Evento onchange a filtro
-filtro.onchange = () => filtrar()
-buscador.onchange = () => buscar()
+const contacto = document.getElementById("contacto")
+const nosotros = document.getElementById("nosotros")
+const principal = document.getElementById("principal")
+const home = document.getElementById("home")
 
-//Verificar que el storage no tenga un carrito y stock de productos guardado y si es asi agregarlo al carrito actual
-let carrito = []
-function verificarLocalStorage(){
-localStorage.getItem("carrito") && (carrito = JSON.parse(localStorage.getItem("carrito"))) ;
-localStorage.getItem("productos") && (productos = JSON.parse(localStorage.getItem("productos")))  ;
+//Eventos
+buscador.onchange = () => buscar()
+contacto.onclick = () => desplegarContacto()
+nosotros.onclick = () => desplegarNosotros()
+terminarCompra.onclick = () => terminandoCompra()
+home.onclick = () => desplegarHome()
+
+//Obtener base de datos con nuestros productos y renderizarlos
+obtenerJsonLocal()
+
+//Antes de entrar se checkea si hay algo guardado en storage, si lo hay se lo agrega al carrito y se calcula el total hasta el momento
+verificarLocalStorage()
+crearCarrito() 
+mostrarTotalCarrito() 
+
+
+//Definiendo las funciones 
+async function obtenerJsonLocal(){
+    const JSON = await fetch('/productos.json') ;
+    const data = await JSON.json() ;
+    productos = data ;
+    verificarLocalStorage()
+    creandoCard(productos)
 }
 
-verificarLocalStorage()
-crearCarrito() //Agrega a la lista de carrito los items que habia on storage
-/* carrito.forEach (producto => {mostrarSinStock(producto)}) */ //agregar si hay stock o no, no me 
-mostrarTotalCarrito() //Calculo de total del carrito on storage y Muestra total
+function verificarLocalStorage(){
+    localStorage.getItem("carrito") && (carrito = JSON.parse(localStorage.getItem("carrito"))) ;
+    localStorage.getItem("productos") && (productos = JSON.parse(localStorage.getItem("productos")))  ;
+}    
 
-
-//Definiendo las funciones del proceso de compra
 function actualizarStock(productoIngresado) {
     productoIngresado.stock = productoIngresado.stock - 1 ;
 }
@@ -98,15 +105,16 @@ function mostrarSinStock(productoElegido) {
 
 function filtrar(){ 
     let seleccion = filtro.value
-    console.log(filtro.value)
     if (seleccion === "precioCreciente"){
         productos.sort((a, b) => a.precio - b.precio);
     }else if(seleccion === "precioDecreciente"){
         productos.sort((a, b) => b.precio - a.precio)
     }else if(seleccion === "alfabetico"){
         productos.sort((a, b) => a.nombre.localeCompare(b.nombre))  
+    }else if(seleccion === "masVendidos"){
+        productos = productos
     }
-    cards.innerHTML = ""
+    principal.innerHTML = ""
     creandoCard(productos)
 }
 
@@ -114,12 +122,105 @@ function buscar() {
     console.log(buscador.value.toLowerCase())
     let productoBuscado = productos.filter(producto => producto.nombre.toLowerCase().includes(buscador.value.toLowerCase()))
     console.log(productoBuscado)
-    cards.innerHTML = ""   
+    principal.innerHTML = ""   
     if
     (productoBuscado.length === 0){Swal.fire('No se encontraron resultados para tu busqueda'),
-    cards.innerHTML = "No se encontraron resultados para tu busqueda",
+    principal.innerHTML = "No se encontraron resultados para tu busqueda",
     buscador.value = ""
     }else{creandoCard(productoBuscado)}
+}
+
+function validacionNombre() {
+    if(isNaN(nombre.value)){nombre.style.color = "black"}else{nombre.style.color = "red"}
+}
+
+function validacionEmail() {
+
+}
+
+function validacionNumero() {
+    if(isNaN(numero.value)){numero.style.color = "red"}else{numero.style.color = "black"}
+}
+
+function validarFormulario(ev) {if(numero.value===""||isNaN(numero.value)||nombre.value===""||!isNaN(nombre.value)||email.value===""||consulta.value==="")
+ {ev.preventDefault();
+    if(numero.value===""||nombre.value===""||email.value===""||consulta.value==="")
+    {Swal.fire('Por favor ingresa todos los datos')}
+    else if(isNaN(numero.value)){Swal.fire('Por favor ingresa un numero de telefono valido')}
+    else if(!isNaN(nombre.value)){Swal.fire('Por favor ingresa un nombre valido')}}
+ else
+ {setTimeout(ev, 5000); Swal.fire({title:'Gracias por tu consulta, te contactaremos a la brevedad', timer: 1500})}
+}
+
+function desplegarContacto() {
+    principal.innerHTML = `
+    <form id="formulario">
+    <div class="mb-3">
+    <label for="email" class="form-label">Direccion de e-mail</label>
+    <input type="email" class="form-control" id="email" placeholder="nombre@ejemplo.com">
+    </div>
+    <div class="mb-3">
+    <label for="nombre" class="form-label" placeholder="Ingresa tu nombre completo">Nombre y apellido</label>
+    <input type="text" class="form-control" id="nombre">
+    </div>
+    <div class="mb-3">
+    <label for="numero" class="form-label" placeholder="Ingresa tu numero de telefono">Numero de telefono</label>
+    <input type="tel" class="form-control" id="numero">
+    </div>
+    <div class="mb-3">
+    <label for="consulta" class="form-label">Contanos en que te podemos ayudar</label>
+    <textarea class="form-control" id="consulta" rows="3"></textarea>
+    </div>    
+    <input type="submit" id="enviar" value="Enviar" class="btn btn-dark col-5 align-self-center" style="width:80px"></input>
+    </form>
+    `
+    principal.className = "row justify-content-center"
+    const email = document.getElementById("email")
+    const nombre = document.getElementById("nombre")
+    const numero = document.getElementById("numero")
+    const consulta = document.getElementById("consulta")
+    const formulario = document.getElementById("formulario")
+    email.oninput = () => validacionEmail()
+    nombre.oninput = () => validacionNombre()
+    numero.oninput = () => validacionNumero()
+    formulario.onsubmit = (ev) => validarFormulario(ev)
+}
+
+function desplegarNosotros() {
+    principal.innerHTML = `
+    <section class="row align-items-center">
+			<main class="col">
+				<h2 class="h2" id="h2">BIENVENIDO!!</h2>
+				<p>Kokekosas en una tienda especialmente creada para ofrecer kokedamas de autor. El arte de hacer kokedamas es una antigua técnica japonesa, “kokedama” significa en japonés bola de musgo (koke = musgo y dama = bola). Esta técnica permite mantener una planta utilizando una maceta viva, acercándote mucho más a la naturaleza. Para nosotros una kokadama es un bosque en miniatura, el olor del musgo nos transporta a estar caminando en un sotobosque, húmedo, sombrío, verde, VIVO! </p>
+			</main>
+	</section>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="Nosotros.jpg" class="d-block w-100" alt="Kokekosas">
+      </div>
+      <div class="carousel-item">
+        <img src="Nosotros2.jpeg" class="d-block w-100" alt="Kokekosas">
+      </div>
+      <div class="carousel-item">
+        <img src="Nosotros3.jpeg" class="d-block w-100" alt="Kokekosas">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>`
+  principal.className = "row justify-content-center"
+}
+
+function desplegarHome() {
+    creandoCard(productos)
+    principal.className= "row widgets justify-content-evenly"
 }
 
 /* function cambiarCantidad() {
@@ -141,7 +242,6 @@ function agregarAlCarrito (productoElegido) {
                 existe.can=existe.can+1;}
             tabla.innerHTML= "" ;
             crearCarrito() ;
-                //agregar alert(`Agregaste ${productoElegido.nombre} al carrito`) pero mas lindos
             actualizarStock(productoElegido) ;
             mostrarTotalCarrito() ;
             guardarEnLocalStorage() ;;
@@ -157,22 +257,33 @@ function agregarAlCarrito (productoElegido) {
 
 //Creando las cards para cada producto Y Evento agregando al carrito la card elegida
 function creandoCard(productos) {
-for (const producto of productos) {
-    const card = document.createElement("div") ;
-    card.innerHTML = `<img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
-      <div class="card-body">                 
-      <h5 class="card-title">${producto.nombre}</h5>
-      <p class="card-text">$${producto.precio}</p>
-      <p id="stock${producto.nombre}" class="card-text stock"></p>
-      <a class="btn btn-dark" href="#table" id="${producto.nombre}btn">Agregar ${producto.nombre} a carrito</a>
-    </div>` ;
-    card.className = "card col-12 col-md-4 col-lg-3" ;
-    cards.appendChild(card) ;
-    document.getElementById(`${producto.nombre}btn`).addEventListener("click",function(){agregarAlCarrito(producto)})
-}
+    principal.innerHTML= `
+        <div class="input-group mb-3" id="filtros">
+        <label class="input-group-text" for="inputGroupSelect01">Filtros</label>
+        <select class="form-select" id="filtro" onchange="filtrar()" >
+        <option id="masVendido" value="masVendidos">Mas vendidos</option>
+        <option value="precioCreciente">Precio creciente</option>
+        <option value="precioDecreciente">Precio decreciente</option>
+        <option value="alfabetico">A - Z</option>
+        </select>
+        </div>`
+    for (const producto of productos) {
+        principal.innerHTML += `
+        <div class="card col-4 col-md-4 col-lg-3">
+        <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+        <div class="card-body">                 
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">$${producto.precio}</p>
+        <p id="stock${producto.nombre}" class="card-text stock"></p>
+        <a class="btn btn-dark" id="${producto.nombre}btn">Agregar ${producto.nombre} a carrito</a>
+        </div>
+        </div>`}
+    productos.forEach(producto => { document.getElementById(`${producto.nombre}btn`).addEventListener("click",function(){agregarAlCarrito(producto)})});   
 }
 
-terminarCompra.onclick = () => { if (carrito.length !== 0){
+
+
+function terminandoCompra() { if (carrito.length !== 0){
     Swal.fire({
    position: 'center',
    icon: 'success',
